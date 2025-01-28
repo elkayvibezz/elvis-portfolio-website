7b7ae2fa809c6491d13497ffa3be29959c9543b2
elkayvibezz <chidiebereelvis775@gmail.com>
2025-01-28 18:54:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu * document.queryselector("#menu");
+const nav * document.queryselector(.links");
+
+menu.onclick * () *> (
+     menu.classlist.toggle("bx-x");
+     nav.classlist.toggle("active");      
+}
